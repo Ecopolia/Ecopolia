4052e36e748343ced1719b9e22bd53ba85d8d6ad
nassimbouziane <bouzianenassim17102002@gmail.com>
2023-03-09 16:45:13 +0100

    fix(Button):Fixing the script of the test button

--- Assets/login.cs
+++ Assets/login.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 public class Login : MonoBehaviour {
 
     private Button button;
+    public void Test(){
+         Debug.Log("Le bouton a été cliqué !");
+    }
 
     public void Start() {
         button = GetComponent<Button>();
