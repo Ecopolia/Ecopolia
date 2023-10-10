764e5f0ff4a5ad3460c10082ba19887463b821ab
Plep-m <menut34@gmail.com>
2023-10-10 11:58:43 +0200

    wip prefab spriterender in ui

--- Assets/UI/UI_BuildMenu_Conroller.cs
+++ Assets/UI/UI_BuildMenu_Conroller.cs
@@ -6,16 +6,11 @@ using UnityEngine.UIElements;
 public class UI_BuildMenu_Conroller : MonoBehaviour
 {
     public Button closeButton;
-
     public Button OpenButton;
-
     public Label buildMenuTitle;
-
     public Button building1;
-
     private bool isOpen;
-
-    public Building[] buildingPrefab;
+    public Building[] buildingPrefab; // Make sure to replace 'Building' with the actual class name of your prefab.
 
     // Start is called before the first frame update
     void Start()
@@ -26,25 +21,43 @@ public class UI_BuildMenu_Conroller : MonoBehaviour
         buildMenuTitle = root.Q<Label>("buildmenu-shop-title");
         building1 = root.Q<Button>("buildmenu-shop-building-card-1");
 
-        // add building prefab image to the menu in the building card
-        Sprite buildingSprite = buildingPrefab[0].GetComponent<SpriteRenderer>().sprite;
-        Texture2D buildingTexture = buildingSprite.texture;
-        building1.style.backgroundImage = new StyleBackground(buildingTexture);
+        // Make sure buildingPrefab array has the prefab assigned.
+        if (buildingPrefab.Length > 0)
+        {
+            // Access the SpriteRenderer of the prefab.
+            SpriteRenderer prefabRenderer = buildingPrefab[0].GetComponent<SpriteRenderer>();
 
+            if (prefabRenderer != null)
+            {
+                // Get the sprite from the SpriteRenderer.
+                Sprite buildingSprite = prefabRenderer.sprite;
+
+                // Create a Texture2D from the sprite's texture.
+                Texture2D buildingTexture = buildingSprite.texture;
+
+                // Set the Button's background image to the Texture2D.
+                building1.style.backgroundImage = new StyleBackground(buildingTexture);
+            }
+            else
+            {
+                Debug.LogError("SpriteRenderer not found on the prefab.");
+            }
+        }
+        else
+        {
+            Debug.LogError("No building prefab assigned in the script.");
+        }
 
         root.Q<VisualElement>("root").style.display = DisplayStyle.None;
         isOpen = false;
         closeButton.clicked += OnCloseButtonClick;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         // change text of the title
-        
     }
-
 
     public void OnCloseButtonClick()
     {
@@ -61,7 +74,6 @@ public class UI_BuildMenu_Conroller : MonoBehaviour
         {
             // Open instantly display flex
             root.Q<VisualElement>("root").style.display = DisplayStyle.Flex;
-            
         }
         else
         {
@@ -71,5 +83,4 @@ public class UI_BuildMenu_Conroller : MonoBehaviour
 
         yield return null; // Ensure that the Coroutine exits immediately
     }
-
 }
