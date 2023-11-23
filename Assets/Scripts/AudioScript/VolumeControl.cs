82b57b302fe8369f9ef77ab6e05812873cec6dbe
Abdo <abdelquodousse@mail.com>
2023-11-23 16:12:41 +0100

    volumecontrol et musicpause script à laisser de cote

--- Assets/Scripts/AudioScript/VolumeControl.cs
+++ Assets/Scripts/AudioScript/VolumeControl.cs
@@ -8,23 +8,19 @@ public class VolumeControl : MonoBehaviour
 
     void Start()
     {
-        // Assurez-vous que l'AudioSource et le Slider sont référencés dans l'éditeur Unity
         if (audioSource == null || volumeSlider == null)
         {
             Debug.LogError("Veuillez attribuer l'AudioSource et le Slider dans l'éditeur Unity.");
             return;
         }
 
-        // Définir la valeur initiale du Slider sur le volume actuel de l'AudioSource
         volumeSlider.value = audioSource.volume;
 
-        // Ajouter un auditeur d'événements pour détecter les changements de valeur du Slider
         volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
     }
 
     void OnVolumeChanged(float volume)
     {
-        // Ajuster le volume de l'AudioSource en fonction de la valeur du Slider
         audioSource.volume = volume;
     }
 }
